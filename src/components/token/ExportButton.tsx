7648ff4fa9db03273f752a01b34f9c1ee66ba9d2
Zilong <r06521609@ntu.edu.twr06521609@ntu.edu.twr06521609@ntu.edu.tw>
2022-10-07 15:10:35 +0800

    refactor : refine the export component
    Signed-off-by: AhWei <aa9440307@gmail.com>

--- src/components/token/ExportButton.tsx
+++ src/components/token/ExportButton.tsx
@@ -1,6 +1,7 @@
 import { Trans } from '@lingui/macro'
-import VerticalAlignBottomIcon from '@mui/icons-material/VerticalAlignBottom'
-import { Box, styled, useTheme } from '@mui/material'
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined'
+import { Button, styled, useTheme } from '@mui/material'
+import { useRef } from 'react'
 import { CSVLink } from 'react-csv'
 
 interface IExportButtonProps {
@@ -8,45 +9,36 @@ interface IExportButtonProps {
   data: any[]
 }
 
-const StyledExportIcon = styled(VerticalAlignBottomIcon)({
+const StyledExportIcon = styled(FileDownloadOutlinedIcon)({
   width: '18px',
   height: '18px',
-  marginRight: '10px',
+  marginRight: '6px',
 })
 
 const ExportButton = ({ fileName, data }: IExportButtonProps) => {
   const theme = useTheme()
+  const csvLink = useRef()
+
+  const handleExportClick = () => {
+    console.log(csvLink)
+    csvLink?.current?.link.click()
+  }
+
   return (
-    <Box
-      sx={{
-        border: `1px solid ${theme.palette.secondary.main}`,
-        borderRadius: '4px',
-        '&:hover': {
-          borderColor: '#528E52',
-          backgroundColor: theme.palette['other'].lightBackground,
-        },
-      }}
+    <Button
+      variant="outlined"
+      sx={{ textTransform: 'uppercase', px: 1 }}
+      onClick={handleExportClick}
     >
+      <StyledExportIcon />
+      <Trans>Export</Trans>
       <CSVLink
-        style={{
-          textTransform: 'uppercase',
-          textDecoration: 'none',
-          color: theme.palette['other'].buttonTextGreen,
-          display: 'flex',
-          alignItems: 'center',
-          fontSize: '13px',
-          fontWeight: '500',
-          padding: '4px 13px',
-          width: '100%',
-          height: '100%',
-        }}
+        style={{ display: 'hide' }}
         data={data}
         filename={fileName}
-      >
-        <StyledExportIcon />
-        <Trans>Export</Trans>
-      </CSVLink>
-    </Box>
+        ref={csvLink}
+      />
+    </Button>
   )
 }
 
