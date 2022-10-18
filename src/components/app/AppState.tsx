e01240abbb202480cb6803c43ab0ec8325f2e528
Zilong <b01501073@g.ntu.edu.tw>
2022-10-18 12:57:55 +0800

    feat : update loading wallet state

--- src/components/app/AppState.tsx
+++ src/components/app/AppState.tsx
@@ -1,9 +1,8 @@
+import LayoutLoadingWallet from '@/components/layout/LayoutLoadingWallet'
 import { ConnectionState, PassphrasePromptReason } from '@chia/api'
 import { useGetKeyringStatusQuery, useGetStateQuery } from '@chia/api-react'
-import { Flex, LayoutHero, LayoutLoading, useSkipMigration } from '@chia/core'
-import { Trans } from '@lingui/macro'
-import { Collapse, Typography } from '@mui/material'
-import React, { ReactNode } from 'react'
+import { LayoutHero } from '@chia/core'
+import { ReactNode } from 'react'
 import AppPassPrompt from './AppPassPrompt'
 
 type Props = {
@@ -12,20 +11,18 @@ type Props = {
 
 export default function AppState(props: Props) {
   const { children } = props
-  const { data: clienState = {}, isLoading: isClientStateLoading } =
+  const { data: clientState = {}, isLoading: isClientStateLoading } =
     useGetStateQuery()
   const { data: keyringStatus, isLoading: isLoadingKeyringStatus } =
     useGetKeyringStatusQuery()
 
   const isConnected =
-    !isClientStateLoading && clienState?.state === ConnectionState.CONNECTED
+    !isClientStateLoading && clientState?.state === ConnectionState.CONNECTED
 
-  if (isLoadingKeyringStatus || !keyringStatus) {
-    return (
-      <LayoutLoading>
-        <Trans>Connecting to Chia Wallet</Trans>
-      </LayoutLoading>
-    )
+  const isLoading = isLoadingKeyringStatus || !keyringStatus || !isConnected
+
+  if (isLoading) {
+    return <LayoutLoadingWallet />
   }
 
   if (keyringStatus?.isKeyringLocked) {
@@ -33,26 +30,6 @@ export default function AppState(props: Props) {
       <LayoutHero>
         <AppPassPrompt reason={PassphrasePromptReason.KEYRING_LOCKED} />
       </LayoutHero>
-    )
-  }
-
-  if (!isConnected) {
-    const { attempt } = clienState
-    return (
-      <LayoutLoading>
-        {!attempt ? (
-          <Trans>Connecting to Chia Wallet</Trans>
-        ) : (
-          <Flex flexDirection="column" gap={1}>
-            <Typography variant="body1" align="center">
-              <Trans>Please Open Chia Wallet</Trans>
-            </Typography>
-            <Typography variant="body1" align="center" color="textSecondary">
-              <Trans>Attempt {attempt}</Trans>
-            </Typography>
-          </Flex>
-        )}
-      </LayoutLoading>
     )
   }
 
---
+++ src/components/layout/LayoutLoadingWallet.tsx
@@ -0,0 +1,60 @@
+import { Trans } from '@lingui/macro'
+import {
+  Alert,
+  AlertTitle,
+  CircularProgress,
+  Stack,
+  Typography,
+  useTheme,
+} from '@mui/material'
+
+const LayoutLoading = () => {
+  const theme = useTheme()
+
+  return (
+    <Stack
+      color={theme.palette.primary.main}
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      position="relative"
+      sx={{
+        height: '100vh',
+      }}
+    >
+      <Alert
+        severity="info"
+        sx={{
+          position: 'absolute',
+          px: '14px',
+          py: '18px',
+          top: '56px',
+          width: '400px',
+        }}
+      >
+        <AlertTitle>
+          <Trans>Login info</Trans>
+        </AlertTitle>
+        <Typography variant="body2">
+          <Trans>
+            Please open your Chia wallet to be logged into your Climate wallet.
+          </Trans>
+        </Typography>
+      </Alert>
+
+      <Typography variant="h4">
+        <Trans>Climate Wallet</Trans>
+      </Typography>
+      <CircularProgress sx={{ marginTop: '50px' }} />
+      <Typography
+        variant="caption"
+        color="textPrimary"
+        sx={{ marginTop: '12px' }}
+      >
+        <Trans>Loading...</Trans>
+      </Typography>
+    </Stack>
+  )
+}
+
+export default LayoutLoading
