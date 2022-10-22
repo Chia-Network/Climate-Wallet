556a6a25c706ccb1071ba96f1dddd37cc58febd1
Zilong <b01501073@g.ntu.edu.tw>
2022-10-22 15:56:17 +0800

    feat : remove alert dialog title

--- src/components/transaction/TransactionPrompt.tsx
+++ src/components/transaction/TransactionPrompt.tsx
@@ -1,23 +1,22 @@
 import usePrompt from '@/hooks/usePrompt'
+import { Trans } from '@lingui/macro'
 import {
   Button,
   Dialog,
   DialogActions,
   DialogContent,
   DialogContentText,
-  DialogTitle,
 } from '@mui/material'
+import { ReactNode } from 'react'
 
 interface TransactionPrompt {
   when: boolean
-  title?: string
-  content?: string
+  content?: string | ReactNode
 }
 
 const TransactionPrompt = ({
   when,
-  title = 'Alert',
-  content = 'Are you sure to leave?',
+  content = <Trans>Are you sure to leave?</Trans>,
 }: TransactionPrompt) => {
   const [showPrompt, setShowPrompt, next] = usePrompt(when)
 
@@ -27,18 +26,19 @@ const TransactionPrompt = ({
 
   return (
     <Dialog open={showPrompt} onClose={handleClose}>
-      <DialogTitle>{title}</DialogTitle>
       <DialogContent>
         <DialogContentText>{content}</DialogContentText>
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleClose}>
+          <Trans>No</Trans>
+        </Button>
         <Button
           onClick={() => {
             next()
           }}
         >
-          Agree
+          <Trans>Yes</Trans>
         </Button>
       </DialogActions>
     </Dialog>
