ce12e2c3156050eff624199f19665a0b13ea50cc
AhWeiHENGuEi <aa9440307@gmail.com>
2022-10-31 16:24:53 +0800

    feat: add custom multiple query for rtk query

---
+++ src/util/multipleCWQuery.ts
@@ -0,0 +1,72 @@
+import { getConfig } from '@/util/yamlConfigLoader'
+import type { BaseQueryFn } from '@reduxjs/toolkit/query'
+import type { AxiosError, AxiosRequestConfig } from 'axios'
+import axios from 'axios'
+
+interface AxiosQuery {
+  url: string
+  method: AxiosRequestConfig['method']
+  data?: AxiosRequestConfig['data']
+  params?: AxiosRequestConfig['params']
+}
+
+const axiosQuery = async ({ url, method, data, params }: AxiosQuery) => {
+  try {
+    const result = await axios({ url: url, method, data, params })
+    return { data: result.data }
+  } catch (axiosError) {
+    let err = axiosError as AxiosError
+    return {
+      error: {
+        status: err.response?.status,
+        data: err.response?.data || err.message,
+      },
+    }
+  }
+}
+
+const multipleCWQuery =
+  (): BaseQueryFn<AxiosQuery, unknown, unknown> =>
+  async ({ url, method, data, params }) => {
+    const serviceUrls = getConfig().climateWarehouses || []
+
+    const resArray = await Promise.all(
+      serviceUrls.map((baseUrl) =>
+        axiosQuery({ url: baseUrl + url, method, data, params })
+      )
+    )
+
+    if (resArray.length > 0) {
+      const reduceData = resArray
+        .map((res) => res?.data)
+        .reduce(
+          (previousValue, currentValue, currentIndex) => {
+            // if currentValue is null skip
+            if (!currentValue) {
+              return previousValue
+            }
+            // if currentIndex=0 set init type array or object
+            let data = previousValue.data
+            if (currentIndex === 0) {
+              data = Array.isArray(currentValue) ? [] : {}
+            }
+
+            if (Array.isArray(currentValue)) {
+              return {
+                data: [...data, ...currentValue],
+              }
+            }
+            return {
+              data: { ...data, ...currentValue },
+            }
+          },
+          { data: undefined }
+        )
+
+      return reduceData
+    }
+
+    return { data: undefined }
+  }
+
+export default multipleCWQuery
